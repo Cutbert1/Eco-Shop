83045602686c339a663fbb5c9c435a02ccf55fb5
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-04-29 16:42:49 +0100

    Add product app and customised product admin

---
+++ products/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'price',
+        'sku',
+        'rating',
+        'created_on',
+        'updated_on',
+        'product_image',
+    )
+    ordering = ('-created_on',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ products/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['name']
+
+    def __str__(self):
+        """Return the name of the category."""
+        return self.name
+
+    def get_friendly_name(self):
+        """Return the friendly name of the category, if available."""
+        return self.friendly_name or self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        "Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    name = models.CharField(max_length=200, unique=True)
+    sku = models.CharField(max_length=200, unique=True, null=True, blank=True)
+    description = models.TextField(verbose_name="Product Description")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    product_image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
