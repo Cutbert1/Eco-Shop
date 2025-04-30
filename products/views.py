f4a66fbcc3f04116ec494c6f13b53cfb101458f3
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-04-30 22:53:19 +0100

    Add query search on product view

--- products/views.py
+++ products/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q  # noqa
+
 from .models import Product
+
 
 # Create your views here.
 
 
-def all_products(request):
-    """ A fetch all products """
-
+def fetch_all_products(request):
+    """Fetch all products with optional search functionality."""
     products = Product.objects.all()
+    search_query = request.GET.get('q', None)
 
-    context = {
-        'products': products
-    }
+    if search_query:
+        if not search_query.strip():
+            messages.error(request, "There was no search parameter")
+            return redirect(reverse('product'))
 
+        products = filter_products_by_query(products, search_query)
+
+    context = create_context(products, search_query)
     return render(request, 'products/products.html', context)
+
+
+def filter_products_by_query(products, query):
+    """Filter products based on the search query."""
+    search_conditions = Q(name__icontains=query) | Q(
+        description__icontains=query
+        )
+    return products.filter(search_conditions)
+
+
+def create_context(products, search_term):
+    """Create context dictionary for rendering."""
+    return {
+        'products': products,
+        'search_term': search_term,
+    }
 
 
 def product_detail(request, product_id):
