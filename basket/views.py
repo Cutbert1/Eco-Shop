cf3aaf60f584d7bc09250988410f2f6f6d866232
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-05-02 01:23:37 +0100

    Add to basket functionality with Javascript

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,34 @@ def view_basket(request):
     """ Renders content of backet page"""
 
     return render(request, 'basket/basket.html')
+
+
+def add_item_to_basket(request, item_id):
+    """Add a specific quantity of an item to the shopping basket."""
+
+    quantity = get_quantity_from_request(request)
+    redirect_url = request.POST.get('redirect_url')
+    basket = get_basket_from_session(request)
+
+    update_basket(basket, item_id, quantity)
+
+    request.session['basket'] = basket
+    log_basket(basket)
+    return redirect(redirect_url)
+
+
+def get_quantity_from_request(request):
+    return int(request.POST.get('quantity', 1))
+
+
+def get_basket_from_session(request):
+    return request.session.get('basket', {})
+
+
+def update_basket(basket, item_id, quantity):
+    basket[item_id] = basket.get(item_id, 0) + quantity
+
+
+def log_basket(basket):
+    """Log the current state of the basket for debugging purposes."""
+    print(basket)
