b6b2fc9d55c05c5cd850a6a47b0a3a6b131ac9ad
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-05-07 12:13:07 +0100

    Strip account registration and add elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,24 @@
+/* Core logic is from strip documentation */
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+
+/* CSS is from strip documentation */
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: 'red',
+        iconColor: 'red'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
         template = 'checkout/checkout.html'
         context = {
             'order_form': order_form,
+            'stripe_public_key': "pk_test_51RM3reISvKrbInR2EBqe5LzOs0JKZrxdvoxlAvLG0ZaAh7GAezJqgoR1cFits3ntI3Ood80Qnfu4dEvEXwrTxw54007H4avRe9",  # noqa
+            'client_secret': 'test client secret',
         }
         return render(request, template, context)
 
