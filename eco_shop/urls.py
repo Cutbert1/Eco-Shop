7ee3eab0b16b824ae9e7b8f08ce3347c87d10c24
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-05-27 20:10:55 +0100

    Add sitemap and robots.txt

--- eco_shop/urls.py
+++ eco_shop/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'eco_shop.views.handler404'
+handler404 = 'eco_shop.views.handler404'  # noqa
